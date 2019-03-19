2a5149bb7f4ff1ae4695f7d1406863df8e80e197
Kalliarolunni <gustigaur@gmail.com>
2019-03-19 14:36:09 +0000

    ha?muna mæta á föst

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,8 +1,11 @@
 #include "robot-config.h"
-
+void taka(){
+    Motor1.spin(directionType::rev,50,velocityUnits::rpm);
+    Motor2.spin(directionType::fwd,50,velocityUnits::rpm);
+}
 int main() {
 while (true) {
-    Vision.takeSnapshot(SIG_2);
+    Vision.takeSnapshot(SIG_7);
     if (Vision.largestObject.exists && Vision.largestObject.width>5) {
         Brain.Screen.setPenColor(vex::color::white);
         Brain.Screen.setFillColor(vex::color::blue);
@@ -10,15 +13,27 @@ while (true) {
         Brain.Screen.setCursor(2,6);
         Brain.Screen.setFont(vex::fontType::mono40);
         Brain.Screen.print("Forward");
-        if (Vision.largestObject.centerX>140 && Vision.largestObject.centerX<150){
-            Motor2.spin(vex::directionType::rev,50,velocityUnits::pct);
-            Motor1.spin(vex::directionType::fwd,50,velocityUnits::pct);
+        if (Vision.largestObject.centerX>130 && Vision.largestObject.centerX<160){
+            if (Vision.largestObject.centerY > 150){
+                Motor1.spin(directionType::fwd,30,velocityUnits::rpm);
+                Motor2.spin(directionType::fwd,30,velocityUnits::rpm);
+                Motor1.spin(directionType::rev,30,velocityUnits::rpm);
+                Motor2.spin(directionType::fwd,30,velocityUnits::rpm);
+            }
+            else if (Vision.largestObject.centerY>140 && Vision.largestObject.centerY < 150){
+                Motor2.spin(vex::directionType::rev,10,velocityUnits::pct);
+                Motor1.spin(vex::directionType::fwd,10,velocityUnits::pct);
+            }
+            else{
+                Motor2.spin(vex::directionType::rev,50,velocityUnits::pct);
+                Motor1.spin(vex::directionType::fwd,50,velocityUnits::pct);
+            }
         }
-        else if(Vision.largestObject.centerX>225){
+        else if(Vision.largestObject.centerX>160){
             Motor2.spin(vex::directionType::fwd,7,velocityUnits::pct);
             Motor1.spin(vex::directionType::fwd,7,velocityUnits::pct);
         }
-        else if(Vision.largestObject.centerX<75){
+        else if(Vision.largestObject.centerX<130){
             Motor2.spin(vex::directionType::rev,7,velocityUnits::pct);
             Motor1.spin(vex::directionType::rev,7,velocityUnits::pct);
         }
@@ -27,7 +42,7 @@ while (true) {
     	else {
             Motor2.stop();
             Motor1.stop();
-            Brain.Screen.clearScreen();
+            Brain.Screen.print(":(");
         }
     task::sleep(100);
 }
